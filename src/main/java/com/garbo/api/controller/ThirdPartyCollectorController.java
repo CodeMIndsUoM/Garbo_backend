7852512cf3ffdeb228db2f2a787c09b3758c8f15
ashmax77 <ashenimalsha7@gmail.com>
2026-02-13 08:16:41 +0530

    Fix ThirdPartyCollectorRepository filename and update controllers

--- src/main/java/com/garbo/api/controller/ThirdPartyCollectorController.java
+++ src/main/java/com/garbo/api/controller/ThirdPartyCollectorController.java
@@ -1,5 +1,6 @@
 package com.garbo.api.controller;
 
+import com.garbo.core.service.ThirdPartyCollectorService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
