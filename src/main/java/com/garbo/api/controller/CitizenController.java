6cc45e392305a3a6c5a160de0d4368dcd281d633
ashmax77 <ashenimalsha7@gmail.com>
2026-02-13 07:57:36 +0530

    Include all changes

--- src/main/java/com/garbo/api/controller/CitizenController.java
+++ src/main/java/com/garbo/api/controller/CitizenController.java
@@ -6,5 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/citizens")
 public class CitizenController {
+    final private CitizenService citizenService;
 
+    public CitizenController(CitizenService citizenService) {
+        this.citizenService = citizenService;
+    }
 }
