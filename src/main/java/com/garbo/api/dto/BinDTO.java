bcddc6ccabd64c6485e10c689f4f0264321c32d7
Kevin200307 <gaveshanasmk.23@uom.lk>
2026-02-17 15:15:14 +0530

    add backend for bin added on map

---
+++ src/main/java/com/garbo/api/dto/BinDTO.java
@@ -0,0 +1,10 @@
+package com.garbo.api.dto;
+
+public class BinDTO {
+
+    public Long id;
+    public double lat;
+    public double lng;
+    public int fillLevel;
+    public String priority;
+}
