414180c5d19b3bbb325401507ca53f87e0edef4f
Hansana Hirunjith <hthhirunjith@gmail.com>
2026-02-17 18:20:25 +0530

    update BinCollectorController.java

--- src/main/java/com/garbo/api/controller/BinCollectorController.java
+++ src/main/java/com/garbo/api/controller/BinCollectorController.java
@@ -1,4 +1,4 @@
-﻿package com.garbo.api.controller;
+package com.garbo.api.controller;
 
 import com.garbo.core.service.BinCollectorService;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/bincollectors")
 public class BinCollectorController {
-    final private BinCollectorService binCollectorService;
+    private final BinCollectorService binCollectorService;
 
     public BinCollectorController(BinCollectorService binCollectorService) {
         this.binCollectorService = binCollectorService;
