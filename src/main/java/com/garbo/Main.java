d6ae5270c1377f2079709c4ea29365f8ff3f9943
Kevin200307 <gaveshanasmk.23@uom.lk>
2026-03-04 10:01:35 +0530

    add route optimization

--- src/main/java/com/garbo/Main.java
+++ src/main/java/com/garbo/Main.java
@@ -1,11 +1,13 @@
 package com.garbo;
 
 import org.springframework.boot.SpringApplication;
+import com.google.ortools.Loader;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
+        Loader.loadNativeLibraries(); 
     }
 }
