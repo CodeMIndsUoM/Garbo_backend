1487c1bed260c7499d37eea00e86f7cc61df188b
wijewardana S.J. <wijewardanasj.23@uom.lk>
2026-03-17 17:33:56 +0530

    Add bin status reporting for field mentors

--- src/main/java/com/garbo/core/service/FieldMentorService.java
+++ src/main/java/com/garbo/core/service/FieldMentorService.java
@@ -2,19 +2,28 @@ package com.garbo.core.service;
 
 import com.garbo.core.entity.FieldMentor;
 import com.garbo.core.repository.FieldMentorRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FieldMentorService {
 
     final private FieldMentorRepository fieldMentorRepository;
+    final private PasswordEncoder passwordEncoder;
 
 
-    public FieldMentorService(FieldMentorRepository fieldMentorRepository) {
+    public FieldMentorService(FieldMentorRepository fieldMentorRepository, PasswordEncoder passwordEncoder) {
         this.fieldMentorRepository = fieldMentorRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public FieldMentor saveFieldMentor(FieldMentor fieldMentor) {
+        fieldMentor.setPassword(passwordEncoder.encode(fieldMentor.getPassword()));
         return this.fieldMentorRepository.save(fieldMentor);
+    }
+
+    public FieldMentor getFieldMentor(Long empId) {
+        return this.fieldMentorRepository.findById(empId)
+                .orElseThrow(() -> new RuntimeException("Field Mentor not found with id: " + empId));
     }
 }
