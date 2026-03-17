e493c1f148678d95c858e6b76e5c60ef59ec60c8
wijewardana S.J. <wijewardanasj.23@uom.lk>
2026-03-17 17:34:12 +0530

    Fix backend warnings

--- src/main/java/com/garbo/api/controller/ThirdPartyCollectorController.java
+++ src/main/java/com/garbo/api/controller/ThirdPartyCollectorController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/thirdpartycollectors")
 public class ThirdPartyCollectorController {
+    @SuppressWarnings("unused")
     private final ThirdPartyCollectorService thirdPartyCollectorService;
 
     public ThirdPartyCollectorController(ThirdPartyCollectorService thirdPartyCollectorService) {
--- src/main/java/com/garbo/infrastructure/config/security/CustomUserDetailsService.java
+++ src/main/java/com/garbo/infrastructure/config/security/CustomUserDetailsService.java
@@ -1,7 +1,5 @@
 package com.garbo.infrastructure.config.security;
 
-import java.util.List;
-
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
