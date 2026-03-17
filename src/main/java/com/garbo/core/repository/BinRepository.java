3227bfcee4fe795793c88c38d6117e4c15db3a16
wijewardana S.J. <wijewardanasj.23@uom.lk>
2026-03-17 17:33:56 +0530

    Add bin status reporting for field mentors

---
+++ src/main/java/com/garbo/core/repository/BinRepository.java
@@ -0,0 +1,12 @@
+package com.garbo.core.repository;
+
+import com.garbo.core.entity.Bin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BinRepository extends JpaRepository<Bin, String> {
+    List<Bin> findByAssignedToEmpId(Long empId);
+}
