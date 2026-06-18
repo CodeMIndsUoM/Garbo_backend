950868290fc5ebeb7369725241112a5f98aa8475
Kevin200307 <gaveshanasmk.23@uom.lk>
2026-06-18 21:43:36 +0530

    add report visibility based on council

--- src/main/java/com/garbo/Main.java
+++ src/main/java/com/garbo/Main.java
@@ -10,7 +10,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
-        Loader.loadNativeLibraries(); 
+        Loader.loadNativeLibraries();
+
     }
 }
 
